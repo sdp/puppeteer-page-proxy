d6acc22e45ad3e285ee467a3945469615ed5135e
Scott Price <scott.price@vaultjs.com>
2022-11-15 15:33:48 -0600

    Added logging

--- src/core/proxy.js
+++ src/core/proxy.js
@@ -6,7 +6,15 @@ const type = require("../util/types");
 // Responsible for applying proxy
 const requestHandler = async (request, proxy, overrides = {}) => {
     // Reject non http(s) URI schemes
+    console.log("requestHandler");
+    console.log("Request");
+    console.log(request);
+    console.log("proxy");
+    console.log(proxy);
+    console.log("overrides");
+    console.log(overrides);
     if (!request.url().startsWith("http") && !request.url().startsWith("https")) {
+        console.log("continuing");
         request.continue(); return;
     }
     const cookieHandler = new CookieHandler(request);
@@ -24,14 +32,18 @@ const requestHandler = async (request, proxy, overrides = {}) => {
         followRedirect: false
     };
     try {
+        console.log("Try");
         const response = await got(overrides.url || request.url(), options);
         // Set cookies manually because "set-cookie" doesn't set all cookies (?)
         // Perhaps related to https://github.com/puppeteer/puppeteer/issues/5364
-        const setCookieHeader = response.headers["set-cookie"];
+        const setCookieHeader = response.headers["set-cookie"];        
         if (setCookieHeader) {
+            console.log("Inside setCookieHeader");
             await cookieHandler.setCookies(setCookieHeader);
             response.headers["set-cookie"] = undefined;
+            console.log("Done setCookieHeader"
         }
+        console.log("Starting request...");
         await request.respond({
             status: response.statusCode,
             headers: response.headers,
@@ -62,6 +74,10 @@ const useProxyPer = {
     HTTPRequest: async (request, data) => {
         let proxy, overrides;
         // Separate proxy and overrides
+        console.log("request");
+        console.log(request);
+        console.log("data");
+        console.log(data);
         if (type(data) === "object") {
             if (Object.keys(data).length !== 0) {
                 proxy = data.proxy;
@@ -70,26 +86,44 @@ const useProxyPer = {
             }
         } else {proxy = data}
         // Skip request if proxy omitted
-        if (proxy) {await requestHandler(request, proxy, overrides)}
-        else {request.continue(overrides)}
+        if (proxy) {
+            console.log("Proxying request:");
+            console.log(request);
+            console.log(proxy);
+            console.log(overrides);
+            await requestHandler(request, proxy, overrides)
+        }
+        else {
+            request.continue(overrides)
+        }
     },
 
     // Call this if page object passed
     CDPPage: async (page, proxy) => {
+        console.log("CDPPAge");
         await page.setRequestInterception(true);
         const listener = "$ppp_requestListener";
         removeRequestListener(page, listener);
         const f = {[listener]: async (request) => {
             await requestHandler(request, proxy);
         }};
-        if (proxy) {page.on("request", f[listener])}
-        else {await page.setRequestInterception(false)}
+        if (proxy) {
+            console.log("Proxying..");
+            page.on("request", f[listener])
+        }
+        else {
+            await page.setRequestInterception(false)
+        }
     }
 }
 
 // Main function
 const useProxy = async (target, data) => {
-    useProxyPer[target.constructor.name](target, data);
+    let name = target.constructor.name;
+    if(name == "Page"){
+        name = "CDPPage"
+    }
+    useProxyPer[name](target, data);
 };
 
 module.exports = useProxy;
